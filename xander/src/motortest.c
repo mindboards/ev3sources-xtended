edab1129a130292e16678a46946d5178ea6a061e
Xander Soldaat <xander@botbench.com>
2013-10-15 11:24:30 +0200

    Nicified the motortest program, now with additional helpfulness!

--- xander/src/motortest.c
+++ xander/src/motortest.c
@@ -8,6 +8,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
 #include "lms2012.h"
 #include "lms_output.h"
 #include "lms_buttons.h"
@@ -16,6 +19,43 @@
 
 char buttonState[BUTTONS];
 
+
+// signal handler to deal with Control-C in the terminal
+void exitAndCleanup(int sig)
+{
+  // clear the screen and update
+  lcdClearScreen();
+  lcdUpdateScreen();
+
+  // make sure all the motors are stopped
+  outputStop(0xFF, 0);
+
+  // Turn off the LED
+  ledSet(LED_BLACK);
+  exit(0);
+}
+
+
+// Draws a nice logo and some helpful text on the screen
+void drawStaticTextLogo()
+{
+  // clear the screen buffer
+  lcdClearScreen();
+
+  // Helpful text
+  lcdDrawStringSmall("LEFT :speed--", 75, 30);
+  lcdDrawStringSmall("RIGHT:speed++", 75, 40);
+  lcdDrawStringSmall("ENTER:stop", 75, 50);
+  lcdDrawStringSmall("BACK :exit", 75, 60);
+
+    // Draw the ROBOTC logo
+  lcdBitBltLCD(logoleft_bits, logoleft_width, logoleft_height, 0, 0, 0, 0, logoleft_width, logoleft_height, RASTER_OP_COPY);
+
+  lcdUpdateScreen();
+}
+
+
+// Initialise the various drivers
 void initAll()
 {
   outputInit();
@@ -31,6 +71,9 @@ void initAll()
   outputStop(0xFF, 0);
   // clear the tacho
   outputClrCount(0x01);
+
+  // Setup the control-C handler
+  signal(SIGINT, exitAndCleanup);
 }
 
 
@@ -42,6 +85,7 @@ int main (int argc, char **argv)
   char textBuff[64];
 
   initAll();
+  drawStaticTextLogo();
 
   // Our main loop
   while (1 == 1)
@@ -61,15 +105,14 @@ int main (int argc, char **argv)
       motorSpeed -= 10;
     else if (buttonState[ENTER_BUTTON -1])
       motorSpeed = 0;
+    else if (buttonState[BACK_BUTTON -1])
+      exitAndCleanup(0);
 
     // Make sure we stay inside the motor speed range
     if (motorSpeed > 100)
       motorSpeed = 100;
     if (motorSpeed < -100)
       motorSpeed = -100;
-
-    // clear the screen buffer
-    lcdClearScreen();
 
     // Pretty up the output
     sprintf(&textBuff[0], "Speed: %d", motorSpeed);
@@ -78,18 +121,6 @@ int main (int argc, char **argv)
     sprintf(&textBuff[0], "Tacho: %d", tachoCount);
     lcdDrawStringSmall(&textBuff[0], 75, 10);
 
-    sprintf(&textBuff[0], "LEFT :speed--");
-    lcdDrawStringSmall(&textBuff[0], 75, 30);
-
-    sprintf(&textBuff[0], "RIGHT:speed++");
-    lcdDrawStringSmall(&textBuff[0], 75, 40);
-
-    sprintf(&textBuff[0], "ENTER:stop");
-    lcdDrawStringSmall(&textBuff[0], 75, 50);
-
-
-    // Draw the ROBOTC logo
-    lcdBitBltLCD(logoleft_bits, logoleft_width, logoleft_height, 0, 0, 0, 0, logoleft_width, logoleft_height, RASTER_OP_COPY);
 
     // Only update if there's been a change
     if (oldMotorSpeed != motorSpeed)
