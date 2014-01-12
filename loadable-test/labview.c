b34df0a0bf68ab0000ae1e6819f4a18701920729
Xander Soldaat <xander@robotc.net>
2014-01-12 11:57:33 +0100

    Loadbale module demo program

---
+++ loadable-test/labview.c
@@ -0,0 +1,21 @@
+/* vim: set tabstop=2 shiftwidth=2 expandtab autoindent: */
+
+/**
+ * Configurable module loading test program
+ *
+ * Changelog:
+ * - 0.1: Initial release
+ *
+ * License: restricted for now
+ *
+ * Xander Soldaat <xander@robotc.net>
+ */
+
+#include <stdio.h>
+
+void viro_init();
+
+void viro_init()
+{
+  printf("hello, I am Labview!\n");
+}
