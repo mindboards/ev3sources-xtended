56887e9d3767c1c69ad29b75788d4c1ab2c810a1
Xander Soldaat <xander@robotc.net>
2014-02-23 20:00:03 +0100

    Program for testing kernel module interfaces

---
+++ ev3sources/lms2012/x_kmodtest/source/kmodtest.c
@@ -0,0 +1,14 @@
+/*
+ * kmodtest.c
+ *
+ *  Created on: Jan 21, 2014
+ *      Author: Xander Soldaat <xander@robotc.net>
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include "c_dynload.h"
+#include "lms2012.h"
+
---
+++ ev3sources/lms2012/x_kmodtest/source/kmodtest.h
@@ -0,0 +1,12 @@
+/*
+ * kmodtest.h
+ *
+ *  Created on: Jan 21, 2014
+ *      Author: Xander Soldaat <xander@kmodtest.net>
+ */
+
+#ifndef KMODTEST_H_
+#define KMODTEST_H_
+
+
+#endif /* KMODTEST_H_ */
